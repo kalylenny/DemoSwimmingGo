fd6083b3e057bdd475e492667a6a9e236af0fcfc
Tri Hung <nthung282@gmail.com>
2019-09-13 18:32:48 +0700

    Dialog add team

--- app/src/main/java/com/example/swimminggo/view/coach/fragment/TeamFragment.java
+++ app/src/main/java/com/example/swimminggo/view/coach/fragment/TeamFragment.java
@@ -23,17 +23,6 @@ public class TeamFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_team, container, false);
-        SwipeMenuListView swipeMenuListView = (SwipeMenuListView) view.findViewById(R.id.listView);
-
-        ArrayList<String> list = new ArrayList<>();
-        list.add("Team 1");
-        list.add("Team 2");
-        list.add("Team 3");
-        list.add("Team 4");
-        list.add("Team 5");
-        ArrayAdapter adapter;
-        adapter = new ArrayAdapter(TeamFragment,android.R.layout.simple_list_item_1,list);
-
         return view;
     }
 }
