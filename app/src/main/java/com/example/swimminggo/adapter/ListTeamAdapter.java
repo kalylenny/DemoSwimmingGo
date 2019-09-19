46e3255cf9fa6343e2a0dbaa00c59ef08a86046f
Tri Hung <nthung282@gmail.com>
2019-09-19 16:50:55 +0700

    UIUX---ListTeam

---
+++ app/src/main/java/com/example/swimminggo/adapter/ListTeamAdapter.java
@@ -0,0 +1,51 @@
+package com.example.swimminggo.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.swimminggo.R;
+import com.example.swimminggo.models.Team;
+
+import java.util.List;
+
+public class ListTeamAdapter extends RecyclerView.Adapter<ListTeamAdapter.TeamViewHolder> {
+    public List<Team> teams;
+
+    public class TeamViewHolder extends RecyclerView.ViewHolder {
+        private TextView teamName, teamAge;
+
+        public TeamViewHolder(View view) {
+            super(view);
+            teamName = (TextView) view.findViewById(R.id.name);
+            teamAge = (TextView) view.findViewById(R.id.age);
+        }
+    }
+
+    public ListTeamAdapter(List<Team> teams) {
+        this.teams=teams;
+    }
+
+    @Override
+    public TeamViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.simple_team_row, parent, false);
+
+        return new TeamViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(TeamViewHolder holder, int position) {
+        Team team = teams.get(position);
+        holder.teamName.setText(team.getTeamName());
+        holder.teamAge.setText(team.getTeamAge());
+    }
+
+    @Override
+    public int getItemCount() {
+        return teams.size();
+    }
+}
+
--- app/src/main/java/com/example/swimminggo/view/coach/fragment/TeamFragment.java
+++ app/src/main/java/com/example/swimminggo/view/coach/fragment/TeamFragment.java
@@ -1,5 +1,6 @@
 package com.example.swimminggo.view.coach.fragment;
 
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,14 +8,26 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.baoyz.swipemenulistview.SwipeMenuListView;
 import com.example.swimminggo.R;
+import com.example.swimminggo.adapter.ListTeamAdapter;
+import com.example.swimminggo.models.Team;
+import com.example.swimminggo.view.coach.SwipeController;
+import com.example.swimminggo.view.coach.SwipeControllerActions;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class TeamFragment extends Fragment {
+
     View view;
+    private ListTeamAdapter mAdapter;
+    SwipeController swipeController = null;
+
     public TeamFragment(){
 
     }
@@ -23,6 +36,77 @@ public class TeamFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_team, container, false);
+        setListTeamAdapter();
+        setupRecyclerView();
         return view;
     }
+
+    private void setListTeamAdapter() {
+        List<Team> team = new ArrayList<>();
+        team.add(new Team("Nhóm bơi nam 1","15"));
+        team.add(new Team("Nhóm bơi nam 1","15"));
+        team.add(new Team("Nhóm bơi nam 1","15"));
+        team.add(new Team("Nhóm bơi nam 1","15"));
+        team.add(new Team("Nhóm bơi nam 1","15"));
+        team.add(new Team("Nhóm bơi nam 1","15"));
+        team.add(new Team("Nhóm bơi nam 1","15"));
+        team.add(new Team("Nhóm bơi nam 1","15"));
+        team.add(new Team("Nhóm bơi nam 1","15"));
+        team.add(new Team("Nhóm bơi nam 1","15"));
+        team.add(new Team("Nhóm bơi nam 1","15"));
+        team.add(new Team("Nhóm bơi nam 1","15"));
+        team.add(new Team("Nhóm bơi nam 1","15"));
+        team.add(new Team("Nhóm bơi nam 1","15"));
+        team.add(new Team("Nhóm bơi nam 1","15"));
+        team.add(new Team("Nhóm bơi nam 1","15"));
+        team.add(new Team("Nhóm bơi nam 1","15"));
+        team.add(new Team("Nhóm bơi nam 1","15"));
+        team.add(new Team("Nhóm bơi nam 1","15"));
+        team.add(new Team("Nhóm bơi nam 1","15"));
+        team.add(new Team("Nhóm bơi nam 1","15"));
+//            String line;
+//            String[] st;
+//            while ((line = reader.readLine()) != null) {
+//                st = line.split(",");
+//                Player player = new Player();
+//                player.setName(st[0]);
+//                player.setNationality(st[1]);
+//                player.setClub(st[4]);
+//                player.setRating(Integer.parseInt(st[9]));
+//                player.setAge(Integer.parseInt(st[14]));
+//                players.add(player);
+//            }
+//        } catch (IOException e) {
+//
+//        }
+
+        mAdapter = new ListTeamAdapter(team);
+    }
+
+    private void setupRecyclerView() {
+        RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.recyclerView);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext(), LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(mAdapter);
+
+        swipeController = new SwipeController(new SwipeControllerActions() {
+            @Override
+            public void onRightClicked(int position) {
+                mAdapter.teams.remove(position);
+                mAdapter.notifyItemRemoved(position);
+                mAdapter.notifyItemRangeChanged(position, mAdapter.getItemCount());
+            }
+        });
+
+        ItemTouchHelper itemTouchhelper = new ItemTouchHelper(swipeController);
+        itemTouchhelper.attachToRecyclerView(recyclerView);
+
+        recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+                swipeController.onDraw(c);
+            }
+        });
+    }
+
 }
