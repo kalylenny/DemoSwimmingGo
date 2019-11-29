67aa273b2cf60c972fcb2b098b22134ea095702d
Tri Hung <nthung282@gmail.com>
2019-11-29 12:43:19 +0700

    Refactor UI

---
+++ app/src/main/java/com/example/swimminggo/view/coach/ListExercise.java
@@ -0,0 +1,16 @@
+package com.example.swimminggo.view.coach;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.swimminggo.R;
+
+public class ListExercise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_exercise);
+    }
+}
